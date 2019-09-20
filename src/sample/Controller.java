92d5df8eaa70da5852e1dbbd671242050e09a392
Daniel Miller <danmil5@yahoo.com>
2019-09-20 14:40:53 -0400

    initial commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+
+  @FXML private Button btnAdd;
+  @FXML private Button btnRecordProd;
+  @FXML private ComboBox<Integer> cboQuantity;
+
+  public void initialize() {
+    for (int i=1; i<=10;i++) {
+      cboQuantity.getItems().add(i);
+      cboQuantity.getSelectionModel().selectFirst();
+      cboQuantity.setEditable(true);
+    }
+  }
+
+  @FXML
+  void AddProductClick(MouseEvent event) {
+    System.out.println("Product has been Added");
+    Main.AddProduct();
+  }
+
+  @FXML
+  void RecordProduction(MouseEvent event) {
+    System.out.println("Production has been Recorded");
+  }
+
+
 }
+
