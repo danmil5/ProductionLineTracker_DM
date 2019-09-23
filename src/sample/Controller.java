c252817f0fd8fd9014fb9aaa85e034f4ae842fa2
Daniel Miller <danmil5@yahoo.com>
2019-09-23 02:44:50 -0400

    insert product name and manufacturer

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -14,11 +15,13 @@ public class Controller {
   @FXML private Button btnAdd;
   @FXML private Button btnRecordProd;
   @FXML private ComboBox<Integer> cboQuantity;
+  @FXML private TextField txtName;
+  @FXML private TextField txtManufacturer;
   final String JDBC_DRIVER = "org.h2.Driver";
   final String DB_URL = "jdbc:h2:./res/prodDB";
-  static Statement stmt = null;
-  static Connection conn = null;
-  static String sql;
+  Statement stmt = null;
+  Connection conn = null;
+  String sql;
 
   public void initialize() {
     for (int i = 1; i <= 10; i++) {
@@ -50,9 +53,14 @@ public class Controller {
     System.out.println("Production has been Recorded");
   }
 
-  public static void AddProduct() {
+  public void AddProduct() {
     try {
-      sql = "INSERT INTO PRODUCT (type, manufacturer, name) VALUES ( 'AUDIO', 'Apple', 'iPod' )";
+      sql =
+          "INSERT INTO PRODUCT (type, manufacturer, name) VALUES ( 'AUDIO', '"
+              + txtManufacturer.getText()
+              + "', '"
+              + txtName.getText()
+              + "' )";
       try {
         stmt.execute(sql);
       } catch (SQLException e) {
