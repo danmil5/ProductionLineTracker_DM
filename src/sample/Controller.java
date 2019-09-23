fefaf692294e39e0ec70a2af3f5e7dbf5384382e
Daniel Miller <danmil5@yahoo.com>
2019-09-23 00:34:48 -0400

    fixed issues for alpha release

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,25 +4,45 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.input.MouseEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Controller {
 
   @FXML private Button btnAdd;
   @FXML private Button btnRecordProd;
   @FXML private ComboBox<Integer> cboQuantity;
+  final String JDBC_DRIVER = "org.h2.Driver";
+  final String DB_URL = "jdbc:h2:./res/prodDB";
+  static Statement stmt = null;
+  static Connection conn = null;
+  static String sql;
 
   public void initialize() {
-    for (int i=1; i<=10;i++) {
+    for (int i = 1; i <= 10; i++) {
       cboQuantity.getItems().add(i);
-      cboQuantity.getSelectionModel().selectFirst();
-      cboQuantity.setEditable(true);
+    }
+    cboQuantity.getSelectionModel().selectFirst();
+    cboQuantity.setEditable(true);
+    try {
+      Class.forName(JDBC_DRIVER);
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+    try {
+      conn = DriverManager.getConnection(DB_URL);
+      stmt = conn.createStatement();
+    } catch (SQLException ex) {
+      ex.printStackTrace();
     }
   }
 
   @FXML
   void AddProductClick(MouseEvent event) {
     System.out.println("Product has been Added");
-    Main.AddProduct();
+    AddProduct();
   }
 
   @FXML
@@ -30,6 +50,16 @@ public class Controller {
     System.out.println("Production has been Recorded");
   }
 
-
+  public static void AddProduct() {
+    try {
+      sql = "INSERT INTO PRODUCT (type, manufacturer, name) VALUES ( 'AUDIO', 'Apple', 'iPod' )";
+      try {
+        stmt.execute(sql);
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    } catch (NullPointerException ex) {
+      ex.printStackTrace();
+    }
+  }
 }
-
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,12 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-import java.sql.*;
-
 public class Main extends Application {
-  final String JDBC_DRIVER = "org.h2.Driver";
-  final String DB_URL = "jdbc:h2:./res/prodDB";
-  static Statement stmt = null;
-  static Connection conn = null;
-  static String sql;
 
   @Override
   public void start(Stage primaryStage) throws Exception {
@@ -23,34 +14,9 @@ public class Main extends Application {
     primaryStage.setTitle("Production Line Tracker");
     primaryStage.setScene(new Scene(root, 300, 275));
     primaryStage.show();
-
-    try {
-      Class.forName(JDBC_DRIVER);
-    } catch (ClassNotFoundException e) {
-      e.printStackTrace();
-    }
-    try {
-      conn = DriverManager.getConnection(DB_URL);
-      stmt = conn.createStatement();
-    } catch (SQLException ex) {
-      ex.printStackTrace();
-    }
   }
 
   public static void main(String[] args) {
     launch(args);
-  }
-
-  public static void AddProduct() {
-    try {
-      sql = "INSERT INTO PRODUCT (type, manufacturer, name) VALUES ( 'AUDIO', 'Apple', 'iPod' )";
-      try {
-        stmt.execute(sql);
-      } catch (SQLException e) {
-        e.printStackTrace();
-      }
-    } catch (NullPointerException ex) {
-      ex.printStackTrace();
-    }
   }
 }
